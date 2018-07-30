841adf19fa72384d9f7f2a5d050cb8f7d50da0cc
neighborpil <neighborpil@naver.com>
2018-07-31 07:47:10 +0900

    task making and cancellation

---
+++ CS_ParallelProgramming/Cancelling Tasks/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancelling_Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cts = new CancellationTokenSource();
+            //var token = cts.Token;
+
+            //token.Register(() =>
+            //{
+            //    Console.WriteLine("Cancelation has been requested");
+            //});
+
+            //var t = new Task(() =>
+            //{
+            //    int i = 0;
+            //    while (true)
+            //    {
+            //        #region soft way of cancelling
+
+            //        //if (token.IsCancellationRequested)
+            //        //    break;
+            //        //else
+            //        //    Console.WriteLine($"{i++}\t");
+            //        #endregion
+            //        #region canonical way of cancelling
+            //        //if (token.IsCancellationRequested)
+            //        //{
+            //        //    throw new OperationCanceledException;
+            //        //} 
+            //        //else
+            //        //    Console.WriteLine($"{i++}\t");
+            //        #endregion
+            //        token.ThrowIfCancellationRequested(); // canonical way
+            //        Console.WriteLine($"{i++}\t");
+            //    }
+            //}, token);
+            //t.Start();
+
+            //Task.Factory.StartNew(() =>
+            //{
+            //    token.WaitHandle.WaitOne();
+            //    Console.WriteLine("Wait handle released, cancelation was requested");
+            //});
+
+            //Console.ReadKey();
+            //cts.Cancel();
+
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+                planned.Token, preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}");
+                    Thread.Sleep(1000);
+                }
+            }, paranoid.Token);
+
+            Console.ReadKey();
+            emergency.Cancel();
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ CS_ParallelProgramming/Testing1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test1 -> using lamda
+            //Task task1 = new Task(() => write('c'));
+            //task1.Start();
+            //Task.Factory.StartNew(() => write(1));
+            //Task.Factory.StartNew(() => write('f', 2));
+
+            // test2 
+            //Task task1 = new Task(write, 'c');
+            //task1.Start();
+            //Task.Factory.StartNew(write, '2');
+            ////Task.Factory.StartNew(write, new object[] { '3', 1 }); 안됨, 안에서 object  나눠줘야
+
+            // test3 - reulst
+            Task<int> task1 = new Task<int>(() => length("Testing"));
+            task1.Start();
+            Task<int> task2 =Task.Factory.StartNew(() => length("test"));
+
+            Console.WriteLine($"Task1 : {task1.Result}");
+            Console.WriteLine($"Task2 : {task2.Result}");
+
+            Console.ReadKey();
+
+        }
+
+        public static int length(object o)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object {o}");
+            return o.ToString().Length;
+        }
+
+
+        public static void write(char c, int i32)
+        {
+            int i = 1000;
+            while(i-- > 0)
+            {
+                Console.Write($"{c}{i32}");
+            }
+        }
+
+        public static void write(char c)
+        {
+            int i = 1000;
+            while(i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        public static void write(object o)
+        {
+            int i = 1000;
+            while(i-- > 0)
+            {
+                Console.Write(o);
+            }
+        }
+    }
+}
