523ae1e3092fec93f522552f03d35503f26ab3cc
박필홍 <feelongpark>
2018-08-08 14:30:49 +0900

    ch2 finished mutex reader-writer lock

---
+++ CH2_Synchronization/CH2_Synchronization/MutextTest2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MutextTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string appName = "MyApp";
+            Mutex mutex;
+
+            try
+            {
+                mutex = Mutex.OpenExisting(appName);
+                Console.WriteLine($"Sorry, {appName} is already running");
+            }
+            catch(WaitHandleCannotBeOpenedException e)
+            {
+                mutex = new Mutex(false, appName);
+                Console.WriteLine("We can run the program just fine");
+            }
+            Console.ReadKey();
+            mutex.ReleaseMutex();
+        }
+    }
+}
---
+++ CH2_Synchronization/CH2_Synchronization/lec13_2_Mutex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lec13_2_Mutex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string appName = "MyApp";
+            Mutex mutex;
+
+            try
+            {
+                mutex = Mutex.OpenExisting(appName);
+                Console.WriteLine($"Sorry, {appName} is already running");
+            }
+            catch (WaitHandleCannotBeOpenedException e)
+            {
+                Console.WriteLine("we can run the program just fine");
+                mutex = new Mutex(false, appName);
+            }
+
+            Console.ReadKey();
+            mutex.ReleaseMutex();
+        }
+    }
+}
